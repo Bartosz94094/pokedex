eb27311c59bef14d26b8144edd147c6bc75a4634
Bartosz Czechowicz <bartosz7czechowicz@gmail.com>
2020-08-05 16:54:32 +0200

    Updates

---
+++ precache-manifest.cb33683c4f296f796f63196ba5e46623.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "60776290776d9ab2ab3e",
+    "url": "/pokedex/static/css/main.777bb398.chunk.css"
+  },
+  {
+    "revision": "60776290776d9ab2ab3e",
+    "url": "/pokedex/static/js/main.8e7d208a.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "b9dc5fd05865636c24d3",
+    "url": "/pokedex/static/css/2.601d100e.chunk.css"
+  },
+  {
+    "revision": "b9dc5fd05865636c24d3",
+    "url": "/pokedex/static/js/2.c3a8b97c.chunk.js"
+  },
+  {
+    "revision": "67e53332f75fec44f8b145713c02c052",
+    "url": "/pokedex/static/media/spinner.67e53332.gif"
+  },
+  {
+    "revision": "e1b54715ed9dc6d86eb2b023f6be608f",
+    "url": "/pokedex/static/media/pokemon.svg.e1b54715.png"
+  },
+  {
+    "revision": "3825123d4c8db9e09b66396aabe467db",
+    "url": "/pokedex/static/media/background2.3825123d.jpg"
+  },
+  {
+    "revision": "d173d93d7c0559ee9505983faf906aaa",
+    "url": "/pokedex/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Bartosz94094/pokedex/precache-manifest.047e2172e1fb0c0dd8e0bdd3563a058d.js"
+  "/pokedex/precache-manifest.cb33683c4f296f796f63196ba5e46623.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Bartosz94094/pokedex/index.html", {
+workbox.routing.registerNavigationRoute("/pokedex/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
