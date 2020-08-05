4cb1f50f46726d7722fa85c52ea460506cff9c67
Bartosz Czechowicz <bartosz7czechowicz@gmail.com>
2020-08-05 15:42:30 +0200

    Updates

---
+++ precache-manifest.047e2172e1fb0c0dd8e0bdd3563a058d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c770769e14f9f3535bcc",
+    "url": "/Bartosz94094/pokedex/static/css/main.d4e8417b.chunk.css"
+  },
+  {
+    "revision": "c770769e14f9f3535bcc",
+    "url": "/Bartosz94094/pokedex/static/js/main.192dcff3.chunk.js"
+  },
+  {
+    "revision": "63ae0065702ca09745de",
+    "url": "/Bartosz94094/pokedex/static/js/runtime~main.d31f08a2.js"
+  },
+  {
+    "revision": "c16f1ff7a33cdc1fba93",
+    "url": "/Bartosz94094/pokedex/static/css/2.601d100e.chunk.css"
+  },
+  {
+    "revision": "c16f1ff7a33cdc1fba93",
+    "url": "/Bartosz94094/pokedex/static/js/2.f5f9964c.chunk.js"
+  },
+  {
+    "revision": "67e53332f75fec44f8b145713c02c052",
+    "url": "/Bartosz94094/pokedex/static/media/spinner.67e53332.gif"
+  },
+  {
+    "revision": "e1b54715ed9dc6d86eb2b023f6be608f",
+    "url": "/Bartosz94094/pokedex/static/media/pokemon.svg.e1b54715.png"
+  },
+  {
+    "revision": "3825123d4c8db9e09b66396aabe467db",
+    "url": "/Bartosz94094/pokedex/static/media/background2.3825123d.jpg"
+  },
+  {
+    "revision": "82b0f5a822c5cea5077559ca02a50401",
+    "url": "/Bartosz94094/pokedex/index.html"
+  }
+];
